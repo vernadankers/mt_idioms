9e30f64e435baea2096230910183fea09bfc2258
vernadankers <vernadankers@gmail.com>
2021-04-27 16:46:30 +0100

    :sparkles: German & Dutch for OS & WMT

--- wsd/data.py
+++ wsd/data.py
@@ -1,18 +1,20 @@
 import pickle
+import tqdm
 from collections import defaultdict
 from torch import FloatTensor as FT
 
 
 class Sentence():
     def __init__(self, sentence, tokenised_sentence, translation, hidden_states, attention,
-                 cross_attention, annotation, tokenised_annotation, pos_tags):
+                 cross_attention, annotation, tokenised_annotation, pos_tags, word_freqs):
         self.sentence = sentence
         self.tokenised_sentence = tokenised_sentence
         self.annotation = annotation
         self.tokenised_annotation = tokenised_annotation
         self.pos_tags = pos_tags
+        self.word_freqs = word_freqs
 
-        self.hidden_states = FT(hidden_states)
+        #self.hidden_states = FT(hidden_states)
         src_len = len(self.tokenised_annotation) + 1
         prd_len = len(translation)
         self.translation = translation
@@ -37,34 +39,36 @@ class Sentence():
                 (i in neighbours if neighbours_only else True)]
 
 
-def extract_sentences():
+def extract_sentences(lang):
     samples = []
     import gc
-    for start, stop in [(0, 1000), (1000, 2000), (2000, 3000), (3000, 4000),
-                        (4000, 5000), (5000, 6000), (6000, 7500)]:
-        pickled_data = pickle.load(open(f"data/contrawsd_{start}-{stop}_pred.pickle", 'rb'))
-        gc.collect()
-        data_info = defaultdict(lambda: dict)
-        for x in open(f"data/contrawsd_{start}-{stop}.tsv", encoding="utf-8"):
-            sentence, annotation, homograph, _, tok_sent, tok_annotation, tok_tags = x.split("\t")
-            data_info[sentence] = {
-                "annotation": annotation,
-                "homograph": homograph,
-                "tok_sent": tok_sent,
-                "tok_annotation": tok_annotation,
-                "tok_tags": tok_tags
-            }
+    for dataset in ["os18", "wmt19"]:
+        for start, stop in [(0, 1000), (1000, 2000), (2000, 3000)]:
+            pickled_data = pickle.load(open(f"data/{dataset}_{start}-{stop}_{lang}_pred.pickle", 'rb'))
+            gc.collect()
+            data_info = defaultdict(lambda: dict)
+            for x in tqdm.tqdm(open(f"data/{dataset}_{start}-{stop}_{lang}.tsv", encoding="utf-8")):
+                sentence, annotation, homograph, _, tok_sent, tok_annotation, tok_tags, word_freqs = x.split("\t")
+                data_info[sentence] = {
+                    "annotation": annotation,
+                    "homograph": homograph,
+                    "tok_sent": tok_sent,
+                    "tok_annotation": tok_annotation,
+                    "tok_tags": tok_tags,
+                    "word_freqs": word_freqs
+                }
 
-        
-        for sent in data_info:
-            samples.append(Sentence(
-                sent,
-                data_info[sent]["tok_sent"],
-                pickled_data["prds"][sent],
-                pickled_data["hidden_states"][sent],
-                pickled_data["attention"][sent],
-                pickled_data["cross_attention"][sent],
-                [int(l) for l in data_info[sent]["annotation"].split()],
-                [int(l) for l in data_info[sent]["tok_annotation"].split()],
-                data_info[sent]["tok_tags"].strip().split()))
+            
+            for sent in data_info:
+                samples.append(Sentence(
+                    sent,
+                    data_info[sent]["tok_sent"],
+                    pickled_data["prds"][sent],
+                    None, #pickled_data["hidden_states"][sent],
+                    pickled_data["attention"][sent],
+                    pickled_data["cross_attention"][sent],
+                    [int(l) for l in data_info[sent]["annotation"].split()],
+                    [int(l) for l in data_info[sent]["tok_annotation"].split()],
+                    data_info[sent]["tok_tags"].strip().split(),
+                    data_info[sent]["word_freqs"]))
     return samples
