c0fd261596cd92c1934b2b79a41f4935b3b0614a
vernadankers <vernadankers@gmail.com>
2021-05-17 00:57:36 +0100

    :sparkles: Added start-stop parameters to attention computation.

--- attention/compute_cross_attention.py
+++ attention/compute_cross_attention.py
@@ -1,97 +1,106 @@
 import sys
-
-sys.path.append('../data/')
-sys.path.append('../probing/')
-
-from data import extract_sentences, Sentence
 from collections import defaultdict
-import torch
-import numpy as np
-import tqdm
-import math
-import seaborn as sns
-import matplotlib.pyplot as plt
 import random
 import argparse
-import pickle
 import logging
-logging.basicConfig(level=logging.INFO)
-
-sentences = extract_sentences(
-    range(0, 50), use_tqdm=True,
-    data_folder="../data/magpie", store_cross_attention=True)
-logging.info("Loaded encodings.")
-
-per_setup = dict()
-for tags in [[], ["NOUN"], ["NOUN", "VERB", "ADJ", "ADV"]]:
-    for neighbourhood in [2, 5, 10]:
-        print("Setup: ", tags, neighbourhood)
-        per_layer = dict()
-        for layer in range(6):
-            avg_attention_con2idi = defaultdict(list)
-            avg_attention_idi2idi = defaultdict(list)
-            avg_attention_idi2con = defaultdict(list)
-            avg_attention_idi2eos = defaultdict(list)
-
-            for sent in sentences:
-                eos_index = sent.translation.index("</s>")
-                translation = sent.translation[:eos_index + 1]
-                num_layers, num_heads, output_length, input_length = sent.cross_attention.shape
-                align_src2tgt = defaultdict(list)
-                align_tgt2src = dict()
-                for i in range(eos_index + 1):
-                    index = torch.argmax(torch.mean(sent.cross_attention[-1, :, i, :-1], dim=0), dim=-1)
-                    align_src2tgt[index.item()].append(i)
-                    align_tgt2src[i] = index.item()
-
-                src_indices = sent.index_select(1, tags=tags)
-                if src_indices:
-                    try:
-                        src_indices = torch.LongTensor(src_indices)
-                        src_indices2 = torch.LongTensor(
-                            sent.index_select(0, tags=tags, neighbours_only=True, neighbourhood=neighbourhood))
-                        tgt_indices = [j for i in src_indices for j in align_src2tgt[i.item()]]
-                        tgt_indices = torch.LongTensor(sorted(list(set(tgt_indices))))
-                        non_tgt_indices = [j for i in src_indices2 for j in align_src2tgt[i.item()]]
-                        non_tgt_indices = torch.LongTensor(sorted(list(set(non_tgt_indices))))
-
-                        att = torch.index_select(sent.cross_attention[layer], dim=-2, index=non_tgt_indices)
-                        att = torch.mean(torch.index_select(att, dim=-1, index=src_indices))
-                        avg_attention_con2idi[sent.magpie_label].append(att.item())
-                        avg_attention_con2idi[sent.translation_label].append(att.item())
-                        avg_attention_con2idi[(sent.magpie_label, sent.translation_label)].append(att.item())
-
-                        att = torch.index_select(sent.cross_attention[layer], dim=-2, index=tgt_indices)
-                        att = torch.mean(torch.index_select(att, dim=-1, index=src_indices))
-                        avg_attention_idi2idi[sent.magpie_label].append(att.item())
-                        avg_attention_idi2idi[sent.translation_label].append(att.item())
-                        avg_attention_idi2idi[(sent.magpie_label, sent.translation_label)].append(att.item())
-
-                        att = torch.index_select(sent.cross_attention[layer], dim=-2, index=tgt_indices)
-                        att = torch.mean(torch.index_select(att, dim=-1, index=src_indices2))
-                        avg_attention_idi2con[sent.magpie_label].append(att.item())
-                        avg_attention_idi2con[sent.translation_label].append(att.item())
-                        avg_attention_idi2con[(sent.magpie_label, sent.translation_label)].append(att.item())
-
-                        att = torch.index_select(sent.cross_attention[layer], dim=-2, index=tgt_indices)
-                        att = torch.mean(torch.index_select(att, dim=-1, index=torch.LongTensor([len(sent.tokenised_annotation)])))
-                        avg_attention_idi2eos[sent.magpie_label].append(att.item())
-                        avg_attention_idi2eos[sent.translation_label].append(att.item())
-                        avg_attention_idi2eos[(sent.magpie_label, sent.translation_label)].append(att.item())
-                    except:
-                        logging.info(f"{sent.cross_attention.shape} {len(sent.tokenised_annotation)}")
-
-                # #Idiom and "aligned" translation
-                # idiom = [sent.tokenised_sentence.split()[i] for i in src_indices]
-                # idiom_translated = [sent.translation[i] for i in tgt_indices]
-                # non_idiom_translated = [sent.translation[i] for i in non_tgt_indices]
-                # print(idiom_translated, non_idiom_translated)
-
-            per_layer[layer] = {"con2idi": avg_attention_con2idi,
-                                "idi2idi": avg_attention_idi2idi,
-                                "idi2con": avg_attention_idi2con,
-                                "idi2eos": avg_attention_idi2eos}
-        per_setup[("_".join(tags) if tags else "all", neighbourhood)] = per_layer
+import pickle
+import torch
+from torch import LongTensor as LT
+sys.path.append('../data/')
+from data import extract_sentences
+random.seed(1)
 
 
-pickle.dump(per_setup, open("cross_attention.pickle", 'wb'))
+def main(mode, start, stop, step):
+    sentences = extract_sentences(
+        range(start, stop, step), use_tqdm=False,
+        data_folder="../data/magpie", store_cross_attention=True)
+
+    # Restrict the data used to...
+    # - identical are matches labelled as identical by MAGPIE
+    # - intersection are PIEs that are both in fig-par and lit-wfw
+    if mode == "identical":
+        sentences = [x for x in sentences if x.variant == "identical"]
+    elif mode == "intersection":
+        lit_idioms = {x.idiom for x in sentences if x.translation_label ==
+                      "word-by-word" and x.magpie_label == "literal"}
+        fig_idioms = {x.idiom for x in sentences if x.translation_label ==
+                      "paraphrase" and x.magpie_label == "figurative"}
+        intersection = lit_idioms.intersection(fig_idioms)
+        sentences = [x for x in sentences if x.idiom in intersection]
+
+    logging.info(f"Processing cross-attention - mode {mode} - {len(sentences)} samples.")
+
+    per_layer = dict()
+    for layer in range(6):
+        idi2idi = defaultdict(list)
+        idi2eos = defaultdict(list)
+        idi2con = defaultdict(list)
+
+        for sent in sentences:
+            # Compute which words align to which
+            eos_index = sent.translation.index("</s>")
+            align_src2tgt = defaultdict(list)
+            for i in range(eos_index + 1):
+                if not  u'▁' in sent.translation[i]:
+                    continue
+                att_last_layer = torch.mean(sent.cross_attention[-1, :, i, :-1], dim=0)
+                index = torch.argmax(att_last_layer, dim=-1)
+                align_src2tgt[index.item()].append(i)
+
+            # Get src PIE indices
+            src_pie_indices = sent.index_select(1, tags=["NOUN"])
+            src_other_indices = [x for x in sent.index_select(1, tags=[])
+                                 if x not in src_pie_indices]
+            if not src_pie_indices:
+                continue
+            if len(src_pie_indices) > 1:
+                src_pie_indices = random.sample(src_pie_indices, 1)
+            # Now align to the words that the PIE terms translated in
+            tgt_indices = [j for i in src_pie_indices for j in align_src2tgt[i]]
+            tgt_indices = LT(sorted(list(set(tgt_indices))))
+
+            # Tgt PIE to src PIE attention
+            att = torch.index_select(sent.cross_attention[layer], dim=-2, index=tgt_indices)
+            att = torch.mean(torch.index_select(att, dim=-1, index=LT(src_pie_indices)))
+            idi2idi[sent.magpie_label].append(att.item())
+            idi2idi[sent.translation_label].append(att.item())
+            idi2idi[(sent.magpie_label, sent.translation_label)].append(att.item())
+
+            # Tgt PIE to src EOS attention
+            att = torch.index_select(sent.cross_attention[layer], dim=-2, index=tgt_indices)
+            att = torch.mean(torch.index_select(
+                att, dim=-1, index=LT([len(sent.tokenised_annotation)])))
+            idi2eos[sent.magpie_label].append(att.item())
+            idi2eos[sent.translation_label].append(att.item())
+            idi2eos[(sent.magpie_label, sent.translation_label)].append(att.item())
+
+            # Tgt PIE to non-PIE in the source attention
+            att = torch.index_select(sent.cross_attention[layer], dim=-2, index=tgt_indices)
+            att = torch.mean(torch.index_select(att, dim=-1, index=LT(src_other_indices)))
+            idi2con[sent.magpie_label].append(att.item())
+            idi2con[sent.translation_label].append(att.item())
+            idi2con[(sent.magpie_label, sent.translation_label)].append(att.item())
+
+            per_layer[layer] = {"idi2idi": idi2idi, "idi2eos": idi2eos,
+                                "idi2con": idi2con}
+
+    if mode == "intersection":
+        pickle.dump(per_layer, open("data/cross_attention_subset=intersection.pickle", 'wb'))
+    elif mode == "identical":
+        pickle.dump(per_layer, open("data/cross_attention_subset=identical.pickle", 'wb'))
+    else:
+        pickle.dump(per_layer, open("data/cross_attention.pickle", 'wb'))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--start", type=int, default=0)
+    parser.add_argument("--stop", type=int, default=100)
+    parser.add_argument("--step", type=int, default=1)
+    parser.add_argument(
+        "--mode", type=str, choices=["regular", "intersection", "identical"],
+        default="regular")
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.INFO)
+    main(args.mode, args.start, args.stop, args.step)
