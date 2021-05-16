2169949c4c51bed564173a49413220e14150efe2
vernadankers <vernadankers@gmail.com>
2021-05-16 20:36:50 +0100

    :bug: Added create folder line.

--- data/preprocess.sh
+++ data/preprocess.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+mkdir magpie
+mkdir magpie/inputs
+
 python preprocess_json.py --pos_tag_method spacy --output_filename idiom_keywords.tsv --frequency_threshold 0.5 --samples_to_file
 wait
 
 #python translate_keywords.py --input_tsv idiom_keywords.tsv --output_tsv idiom_keywords_translated.tsv
 #wait
 
-mkdir magpie
 mkdir magpie/hidden_states_enc
 mkdir magpie/hidden_states_dec
 mkdir magpie/attention
