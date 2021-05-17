98ac35442a26708102b73bb0f7d613c370280fc2
vernadankers <vernadankers@gmail.com>
2021-05-17 18:18:45 +0100

    :recycle: Changed probing to logistic regression classifiers.

--- amnesic_probing/probing.py
+++ amnesic_probing/probing.py
@@ -1,10 +1,8 @@
 import sys
 sys.path.append('../data/')
-sys.path.append("../probing")
-from train_probes import set_seed
 import random
 import torch
-from classifier import Classifier
+import pickle
 from data import extract_sentences
 import numpy as np
 from collections import defaultdict
@@ -16,27 +14,45 @@ from debias import get_debiasing_projection, SKlearnClassifier, load_data
 from sklearn.linear_model import LogisticRegression
 
 
+def set_seed(seed):
+    """Set random seed."""
+    if seed == -1:
+        seed = random.randint(0, 1000)
+    logging.info(f"Seed: {seed}")
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+
+    # if you are using GPU
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.enabled = False
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--m", type=int, default=0)
-    parser.add_argument("--n", type=int, default=100)
-    parser.add_argument("--k", type=int, default=1)
-    parser.add_argument("--l", type=int, default=200)
+    parser.add_argument("--start", type=int, default=0)
+    parser.add_argument("--stop", type=int, default=100)
+    parser.add_argument("--step", type=int, default=1)
     parser.add_argument("--setup", type=str, choices=["attention", "hidden"])
     parser.add_argument("--target", type=str, choices=["both", "magpie"])
     parser.add_argument("--average_pie", action="store_true")
+    parser.add_argument("--split_by_idiom", action="store_true")
     args = parser.parse_args()
     print(vars(args))
 
     set_seed(333)
     # Load all hidden representations of idioms
     data = dict()
-    for i in range(args.m, args.n, args.k):
+    for i in range(args.start, args.stop, args.step):
         if (i + 1) % 50 == 0:
-            logging.info(f"Sample {i/args.k:.0f} / {(args.n-args.m)/args.k:.0f}")
+            logging.info(f"Loading idiom {i/args.step:.0f} / {(args.stop-args.start)/args.step:.0f}")
         samples = extract_sentences(
-            [i], use_tqdm=False, store_hidden_states=True, store_attention_query=True)
+            [i], use_tqdm=False, store_hidden_states=args.setup == "hidden",
+            store_attention_query=args.setup == "attention")
         for s in samples:
             if args.target == "both":
                 if s.translation_label == "paraphrase" and s.magpie_label == "figurative":
@@ -60,12 +76,23 @@ if __name__ == "__main__":
     indices = list(data.keys())
     random.shuffle(indices)
 
-    n = int(len(indices)/5)
-    fold_1 = [s for i in indices[:n] for s in data[i]]
-    fold_2 = [s for i in indices[n:n*2] for s in data[i]]
-    fold_3 = [s for i in indices[n*2:n*3] for s in data[i]]
-    fold_4 = [s for i in indices[n*3:n*4] for s in data[i]]
-    fold_5 = [s for i in indices[n*4:] for s in data[i]]
+    if args.split_by_idiom:
+        n = int(len(indices)/5)
+        fold_1 = [s for i in indices[:n] for s in data[i]]
+        fold_2 = [s for i in indices[n:n*2] for s in data[i]]
+        fold_3 = [s for i in indices[n*2:n*3] for s in data[i]]
+        fold_4 = [s for i in indices[n*3:n*4] for s in data[i]]
+        fold_5 = [s for i in indices[n*4:] for s in data[i]]
+
+    else:
+        samples = [s for i in indices for s in data[i]]
+        n = int(len(samples)/5)
+        random.shuffle(samples)
+        fold_1 = [s for s in samples[:n]]
+        fold_2 = [s for s in samples[n:n*2]]
+        fold_3 = [s for s in samples[n*2:n*3]]
+        fold_4 = [s for s in samples[n*3:n*4]]
+        fold_5 = [s for s in samples[n*4:]]
 
     train_1 = fold_2 + fold_3 + fold_4 + fold_5
     test_1 = fold_1
@@ -85,9 +112,15 @@ if __name__ == "__main__":
     folds = [(train_1, test_1), (train_2, test_2), (train_3, test_3),
              (train_4, test_4), (train_5, test_5)]
 
+    scores_dict = defaultdict(lambda: defaultdict(list))
     for layer in range(6 if args.setup == "attention" else 7):
-        f1s, baseline_f1s = [], []
+        f1s_train, f1s_test, baseline_f1s = [], [], []
         for train, test in folds:
+            train_sentences = [s.sentence+s.idiom for s in train]
+            test_sentences = [s.sentence+s.idiom for s in test]
+            assert not set(train_sentences).intersection(set(test_sentences)), \
+                len(set(train_sentences).intersection(set(test_sentences)))
+
             X_train, Y_train, median_freq = load_data(
                 train, layer, args.setup == "attention", False,
                 average_pie=args.average_pie)
@@ -95,8 +128,25 @@ if __name__ == "__main__":
                 test, layer, args.setup == "attention", False,
                 median_freq, average_pie=args.average_pie)
             clf = SKlearnClassifier(LogisticRegression(**{"max_iter": 5000, "random_state": 1}))
-            f1_macro = clf.train_network(X_train, Y_train, X_dev, Y_dev)
+            f1_train, f1_test = clf.train_network(X_train, Y_train, X_dev, Y_dev)
+            f1_baseline = f1_score(Y_dev, [random.choice(Y_train) for _ in Y_dev], average="macro")
 
-            f1s.append(f1_macro)
-            baseline_f1s.append(f1_score(Y_dev, [random.choice(Y_train) for _ in Y_dev], average="macro"))
-        logging.info(f"{layer} {np.mean(f1s):.3f} {np.std(f1s)}, {np.mean(baseline_f1s):.3f}")
+            scores_dict[layer]["f1_train"].append(f1_train)
+            scores_dict[layer]["f1_test"].append(f1_test)
+            scores_dict[layer]["f1_baseline"].append(f1_baseline)
+
+            f1s_train.append(f1_train)
+            f1s_test.append(f1_test)
+            baseline_f1s.append(f1_baseline)
+
+
+        logging.info(f"{layer}, {np.mean(f1s_train):.3f} {np.std(f1s_train):.3f}, " +
+                     f"{np.mean(f1s_test):.3f}+/-{np.std(f1s_test):.3f}, " +
+                     f"{np.mean(baseline_f1s):.3f}")
+
+    for x in scores_dict:
+        scores_dict[x] = dict(scores_dict[x])
+    scores_dict = dict(scores_dict)
+    pickle.dump(
+        scores_dict,
+        open(f"data/f1s_setup={args.setup}_target={args.target}_average={args.average_pie}.pickle", 'wb'))
