a9513d8d639a7a48040e4c974448c1dcb71a78b5
vernadankers <vernadankers@gmail.com>
2021-05-17 22:49:32 +0100

    :bug: Removed stop at 80 from probing.

--- amnesic_probing/run_probing.sh
+++ amnesic_probing/run_probing.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-python probing.py --start 0 --stop 1726 --step 1 --setup hidden --target both --split_by_idiom --stop 80
+python probing.py --start 0 --stop 1726 --step 1 --setup hidden --target both --split_by_idiom
 wait
-python probing.py --start 0 --stop 1726 --step 1 --setup attention --target both --split_by_idiom --stop 80
+python probing.py --start 0 --stop 1726 --step 1 --setup attention --target both --split_by_idiom
 wait
-python probing.py --start 0 --stop 1726 --step 1 --setup hidden --target both --split_by_idiom --stop 80 --average_pie
+python probing.py --start 0 --stop 1726 --step 1 --setup hidden --target both --split_by_idiom --average_pie
 wait
-python probing.py --start 0 --stop 1726 --step 1 --setup hidden --target magpie --split_by_idiom --stop 80 
+python probing.py --start 0 --stop 1726 --step 1 --setup hidden --target magpie --split_by_idiom
 wait
-python probing.py --start 0 --stop 1726 --step 1 --setup attention --target magpie --split_by_idiom --stop 80
+python probing.py --start 0 --stop 1726 --step 1 --setup attention --target magpie --split_by_idiom
 wait
-python probing.py --start 0 --stop 1726 --step 1 --setup hidden --target magpie --split_by_idiom --stop 80 --average_pie
+python probing.py --start 0 --stop 1726 --step 1 --setup hidden --target magpie --split_by_idiom --average_pie
 wait
