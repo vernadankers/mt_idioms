e6ac17f9e3674d31445240ae585f4b8a92f4fefb
vernadankers <vernadankers@gmail.com>
2021-05-18 10:14:02 +0100

    :art: Clean-up

--- hidden_representations/over_layers.py
+++ hidden_representations/over_layers.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('../data/')
-sys.path.append('../probing/')
+sys.path.append('../amnesic_probing/')
+from probing import set_seed
 import os
 import random
 from collections import defaultdict, Counter
@@ -14,7 +15,7 @@ import logging
 import pickle
 
 logging.basicConfig(level=logging.INFO)
-
+set_seed(1)
 
 
 def compute_cosine_sim(data1, data2, cca_results):
@@ -40,96 +41,70 @@ def compute_cosine_sim(data1, data2, cca_results):
     return torch.mean(cos).item()
 
 
-# Load all hidden representations of idioms
-samples_for_cca = extract_sentences(
-    range(0, 1727), use_tqdm=True, data_folder="../data/magpie",
-    store_hidden_states=True, get_verb_idioms=True)
-logging.info(len(samples_for_cca))
-sents1 = [s.sentence for s in samples_for_cca]
+def main():
+    # Load all hidden representations of idioms
+    samples_for_cca = extract_sentences(
+        range(0, 1727), use_tqdm=True, data_folder="../data/magpie",
+        store_hidden_states=True, get_verb_idioms=True)
+    logging.info(len(samples_for_cca))
+    sents1 = [s.sentence for s in samples_for_cca]
 
-samples_to_analyse = extract_sentences(
-    range(0, 1727), use_tqdm=True, data_folder="../data/magpie",
-    store_hidden_states=True)
-logging.info(len(samples_to_analyse))
-sents2 = [s.sentence for s in samples_to_analyse]
-logging.info(len(set(sents1).intersection(set(sents2))))
+    samples_to_analyse = extract_sentences(
+        range(0, 1727), use_tqdm=True, data_folder="../data/magpie",
+        store_hidden_states=True)
+    logging.info(len(samples_to_analyse))
+    sents2 = [s.sentence for s in samples_to_analyse]
+    logging.info(len(set(sents1).intersection(set(sents2))))
 
-# Perform CCA on a separate set of samples
+    # Perform CCA on a separate set of samples
+    cca_results = []
+    random.shuffle(samples_for_cca)
+    for i in range(1, 7):
+        layer1 = []
+        layer2 = []
+        for s in samples_for_cca:
+            layer1.extend(s.hidden_states[i - 1].tolist())
+            layer2.extend(s.hidden_states[i].tolist())
+        layer1 = torch.FloatTensor(layer1).transpose(0, 1).numpy()
+        layer2 = torch.FloatTensor(layer2).transpose(0, 1).numpy()
+        cca = cca_core.get_cca_similarity(
+            layer1[:, :100000], layer2[:, :100000], epsilon=1e-6)
+        cca_results.append(cca)
 
-per_setup_labels = defaultdict(list)
-
-cca_results = []
-random.shuffle(samples_for_cca)
-for i in range(1, 7):
-    layer1 = []
-    layer2 = []
-    for s in samples_for_cca:
-        layer1.extend(s.hidden_states[i-1].tolist())
-        layer2.extend(s.hidden_states[i].tolist())
-    layer1 = torch.FloatTensor(layer1).transpose(0, 1).numpy()
-    layer2 = torch.FloatTensor(layer2).transpose(0, 1).numpy()
-    cca = cca_core.get_cca_similarity(
-        layer1[:, :100000], layer2[:, :100000], epsilon=1e-6)
-    cca_results.append(cca)
-
-for TAGS in [["NOUN"]]:
     # Now analyse the remaining samples
     all_coefs = defaultdict(list)
-    freq_subsets = defaultdict(lambda: defaultdict(list))
+    hidden_states = defaultdict(lambda: defaultdict(list))
     random.seed(1)
     random.shuffle(samples_to_analyse)
     for layer_ in range(7):
         for s in samples_to_analyse:
-            for word, hidden_state, a, tag in zip(
-                s.tokenised_sentence.split(), s.hidden_states[layer_],
-                s.tokenised_annotation, s.pos_tags):
-                if a == 0 or (tag not in TAGS if TAGS else False):
-                    continue
-                freq_subsets[layer_][(s.magpie_label, s.translation_label)].append(hidden_state)
-                freq_subsets[layer_][s.magpie_label].append(hidden_state)
-                freq_subsets[layer_][s.translation_label].append(hidden_state)
+            # Collect PIE nouns
+            for i in s.index_select(1, tags=["NOUN"]):
+                h = s.hidden_states[layer_][i]
+                hidden_states[layer_][(1, s.magpie_label, s.translation_label)].append(h)
+                hidden_states[layer_][(1, s.magpie_label)].append(h)
+                hidden_states[layer_][(1, s.translation_label)].append(h)
 
-    for category in freq_subsets[0].keys():
+            # And non-PIE nouns
+            for i in s.index_select(0, tags=["NOUN"]):
+                h = s.hidden_states[layer_][i]
+                hidden_states[layer_][(0, s.magpie_label, s.translation_label)].append(h)
+                hidden_states[layer_][(0, s.magpie_label)].append(h)
+                hidden_states[layer_][(0, s.translation_label)].append(h)
+
+    for category in hidden_states[0].keys():
         coefs = []
         for i in range(1, 7):
-            indices = list(range(len(freq_subsets[i][category])))
+            indices = list(range(len(hidden_states[i][category])))
             random.shuffle(indices)
             indices = indices[:100000]
-            layer1 = torch.stack(freq_subsets[i-1][category], dim=0)[indices].transpose(0, 1).numpy()
-            layer2 = torch.stack(freq_subsets[i][category], dim=0)[indices].transpose(0, 1).numpy()
+            layer1 = torch.stack(hidden_states[i - 1][category], dim=0)[indices].transpose(0, 1).numpy()
+            layer2 = torch.stack(hidden_states[i][category], dim=0)[indices].transpose(0, 1).numpy()
             coefs.append(compute_cosine_sim(layer1, layer2, cca_results[i - 1]))
         all_coefs[category] = coefs
 
-    per_setup_labels[(1, "_".join(TAGS) if TAGS else "all")] = dict(all_coefs)
+    pickle.dump(dict(all_coefs), open("data/over_layers.pickle", "wb"))
 
-for TAGS in [["NOUN"]]:
-    # Now analyse the remaining samples
-    all_coefs = defaultdict(list)
-    freq_subsets = defaultdict(lambda: defaultdict(list))
-    random.seed(1)
-    random.shuffle(samples_to_analyse)
-    for layer_ in range(7):
-        for s in samples_to_analyse:
-            for word, hidden_state, a, tag in zip(
-                s.tokenised_sentence.split(), s.hidden_states[layer_],
-                s.tokenised_annotation, s.pos_tags):
-                if a == 1 or (tag not in TAGS if TAGS else False):
-                    continue
-                freq_subsets[layer_][(s.magpie_label, s.translation_label)].append(hidden_state)
-                freq_subsets[layer_][s.magpie_label].append(hidden_state)
-                freq_subsets[layer_][s.translation_label].append(hidden_state)
 
-    for category in freq_subsets[0].keys():
-        coefs = []
-        for i in range(1, 7):
-            indices = list(range(len(freq_subsets[i][category])))
-            random.shuffle(indices)
-            indices = indices[:100000]
-            layer1 = torch.stack(freq_subsets[i-1][category], dim=0)[indices].transpose(0, 1).numpy()
-            layer2 = torch.stack(freq_subsets[i][category], dim=0)[indices].transpose(0, 1).numpy()
-            coefs.append(compute_cosine_sim(layer1, layer2, cca_results[i - 1]))
-        all_coefs[category] = coefs
-
-    per_setup_labels[(0, "_".join(TAGS) if TAGS else "all")] = dict(all_coefs)
-
-pickle.dump(per_setup_labels, open("data/over_layers.pickle", "wb"))
+if __name__ == "__main__":
+    main()
