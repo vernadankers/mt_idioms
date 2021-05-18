c15973b80d2d6b7cead76a2b1e86102ba369453c
vernadankers <vernadankers@gmail.com>
2021-05-18 10:11:06 +0100

    :sparkles: Updated translate script for WSD to match transformers version.

--- wsd/translate_wsd.py
+++ wsd/translate_wsd.py
@@ -4,7 +4,7 @@ import nltk
 import torch
 import pickle
 
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+from transformers import MarianTokenizer, MarianMTModel
 
 
 if __name__ == "__main__":
@@ -15,14 +15,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--source", type=str, required=True)
     parser.add_argument("--pred", type=str, required=True)
+    parser.add_argument("--lang", type=str, default="nl")
     args = vars(parser.parse_args())
 
     src = 'en'  # source language
-    trg = 'nl'  # target language
+    trg = args["lang"]  # target language
 
     mname = f'Helsinki-NLP/opus-mt-{src}-{trg}'
-    model = AutoModelForSeq2SeqLM.from_pretrained(mname)
-    tok = AutoTokenizer.from_pretrained(mname)
+    model = MarianMTModel.from_pretrained(mname)
+    tok = MarianTokenizer.from_pretrained(mname)
+    if torch.cuda.is_available():
+        model = model.cuda()
 
     TEST_FILE_SRC = args["source"]
     PRED_FILE = args["pred"]
@@ -47,9 +50,12 @@ if __name__ == "__main__":
                 logging.info(f"Doing {i:4d}/{corpus_length:4d}")
                 batch = tok.prepare_seq2seq_batch(
                     src_texts=srcs, return_tensors="pt")
+                if torch.cuda.is_available():
+                    for x in batch:
+                        batch[x] = batch[x].cuda()
 
                 outputs_beam = model.generate(
-                    **batch, num_beams=5, return_dict_in_generate=True,
+                    **batch, num_beams=5, return_dict_in_generate=True, max_length=512,
                     output_hidden_states=True, output_attentions=True)
 
                 outputs_cross_attention = model.forward(
@@ -58,9 +64,9 @@ if __name__ == "__main__":
                     decoder_input_ids=outputs_beam["sequences"])
 
                 cross_attention_batch = torch.stack(
-                    outputs_cross_attention["cross_attentions"], dim=0).transpose(0, 1)
+                    [x[0] for x in outputs_cross_attention["cross_attentions"]], dim=0).transpose(0, 1)
                 encoder_attention_batch = torch.stack(
-                    outputs_beam["encoder_attentions"], dim=0).transpose(0, 1)
+                    [x[0] for x in outputs_beam["encoder_attentions"]], dim=0).transpose(0, 1)
                 hidden_states_tmp_batch = torch.stack(
                     outputs_beam["encoder_hidden_states"], dim=0).transpose(0, 1)
 
@@ -70,7 +76,7 @@ if __name__ == "__main__":
                         src_annotations.extend(["_"] * len(tok.tokenize(w)))
 
                     src_len = len(src_lengths[j].split()) + 1
-                    assert src_len == len(src_annotations) + 1
+                    assert src_len == len(src_annotations) + 1, (src_len, len(src_annotations) + 1)
 
                     prd = tok.convert_ids_to_tokens(outputs_beam["sequences"][j])[1:]
                     cross_attention[srcs[j]] = \
@@ -87,7 +93,6 @@ if __name__ == "__main__":
         "prds": prds,
         "attention": encoder_attention,
         "cross_attention": cross_attention,
-        "hidden_states": hidden_states
     }
 
     pickle.dump(all_encodings, open(PRED_FILE.replace("txt", "pickle"), 'wb'))
