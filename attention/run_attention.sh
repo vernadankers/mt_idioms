d321c26e9d14636be56f8f4fd0f507aaaac83e67
Verna <vernadankers@gmail.com>
2021-11-15 21:49:34 +0000

    :recycle: Cleanup.

--- attention/run_attention.sh
+++ attention/run_attention.sh
@@ -2,17 +2,11 @@
 
 for language in nl de sv da fr it es
 do
-    python compute_cross_attention.py --mode regular --start 0 --stop 1727 --language $language --use_precomputed_alignments --folder data_precomputed
+    python compute_cross_attention.py --mode regular --start 0 --stop 1727 --language $language --use_precomputed_alignments
     wait
-    python compute_cross_attention.py --mode intersection --start 0 --stop 1727 --language $language --use_precomputed_alignments --folder data_precomputed
+    python compute_cross_attention.py --mode intersection --start 0 --stop 1727 --language $language --use_precomputed_alignments
     wait
-    python compute_cross_attention.py --mode identical --start 0 --stop 1727 --language $language --use_precomputed_alignments --folder data_precomputed
-    wait
-    python compute_cross_attention.py --mode regular --start 0 --stop 1727 --language $language
-    wait
-    python compute_cross_attention.py --mode intersection --start 0 --stop 1727 --language $language
-    wait
-    python compute_cross_attention.py --mode identical --start 0 --stop 1727 --language $language
+    python compute_cross_attention.py --mode identical --start 0 --stop 1727 --language $language --use_precomputed_alignments
     wait
     python compute_attention.py --mode regular --start 0 --stop 1727 --language $language
     wait
