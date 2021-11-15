53bd9a6e25222cd88c39435481a15f2cbd6932c4
Verna <vernadankers@gmail.com>
2021-11-15 10:57:47 +0000

    :sparkles: Adapted to fit multiple languages.

--- data/classifier.py
+++ data/classifier.py
@@ -1,71 +1,117 @@
 import spacy
+import inflect
+import pyinflect
 import dutch_words
-from translate_keywords import to_plural_singular
+import nltk
+import stanza
+import spacy_stanza
 
+inflect = inflect.engine()
 
 class Classifier():
     """Simple classifier to label translations according to whether they
     contain the translation of a keywor (or not)."""
 
-    def __init__(self, idiom_file: str):
+    def __init__(self, idiom_file: str, exclude_deepl: bool = False, use_spacy: bool = False, language: str = "nl"):
         self.idioms = []
-        self.nl_keywords = dict()
-        self.en_keywords = dict()
+        self.src_keywords = dict()
+        self.tgt_keywords = dict()
 
+        self.use_spacy = use_spacy
         self.nlp_en = spacy.load("en_core_web_sm")
-        self.nld_words = dutch_words.get_ranked()
+        if use_spacy:
+            self.nlp_tgt = spacy.load(f"{language}_core_news_sm")
 
         # Open the idioms that contain nouns from file
         with open(idiom_file, encoding="utf-8") as f_annot:
             for line in f_annot:
-                if len(line.strip().split("\t")) != 5:
-                    continue
-                idiom, inclusion, _, en_keywords, nl_keywords = \
-                    line.strip().split("\t")
-                self.idioms.append(idiom)
+
+                if len(line.split("\t")) == 5:
+                    if line.strip().split("\t")[1] == "no":
+                        continue
+                    idiom, inclusion, _, src_keywords, marian_keywords = \
+                        line.strip().split("\t")
+                    deepl_keywords = ""
+                else:
+                    idiom, inclusion, _, src_keywords, deepl_keywords, marian_keywords = \
+                        line.strip().split("\t")
                 if inclusion == "yes":
-                    self.nl_keywords[idiom] = nl_keywords.split()
-                    self.en_keywords[idiom] = en_keywords.split()
+                    self.idioms.append(idiom)
+
+                    self.tgt_keywords[idiom] = self.preprocess_keywords(
+                        marian_keywords.lower().split())
+                    if not exclude_deepl:
+                        self.tgt_keywords[idiom].update(
+                            self.preprocess_keywords(
+                                deepl_keywords.lower().split()))
+
+                    self.src_keywords[idiom] = src_keywords.lower().split()
 
     def __call__(self, idiom: str, prd: str):
         """Return the classification of the translation for this idiom."""
         return self.classify(idiom, prd)
 
+    def preprocess_keywords(self, keywords):
+        unique_keywords = set()
+        for keyword in keywords:
+            unique_translations = set()
+            for translation in keyword.split(";"):
+                translation = translation.split('_')
+                if len(translation) == 2:
+                    translation1, translation2 = translation
+                elif len(translation) == 3:
+                    translation1, translation2, _ = translation
+                else:
+                    translation1 = translation[0]
+                    translation2 = translation[0]
+                translation1 = translation1.replace("&#39;", "'")
+                translation2 = translation2.replace("&#39;", "'")
+                if translation1.lower() and translation1.lower(): # self.nld_words[42:200]:
+                    unique_translations.add(translation1.lower())
+                if translation2.lower() and translation2.lower():
+                    unique_translations.add(translation2.lower())
+            unique_keywords.add(';'.join(list(unique_translations)))
+        return unique_keywords
+
     def contains(self, idiom: str):
         """Check whether the classifier contains keywords for this idiom."""
-        return idiom in self.nl_keywords
+        return idiom in self.tgt_keywords
 
     def classify(self, idiom: str, prd: str):
         """For a given idiom, predict the class of the predicted translation."""
         prd = prd.lower().strip()
         idiom = idiom.lower().strip()
+        if idiom not in self.idioms:  # not self.tgt_keywords[idiom]:
+            return "none"
 
-        # Voorzitter... is a common Europarl hallucination, exclude them :-(
-        if idiom not in self.nl_keywords or not prd or "Voorzitter" in prd:
+        if self.use_spacy:
+            lemmas = []
+            for w in self.nlp_tgt(prd):
+                lemmas.append(w.lemma_)
+            prd = prd + " " + " ".join(lemmas)
+
+        if idiom not in self.tgt_keywords:
             return "none"
 
         # Check if the Englis keyword is present
-        keywords = self.en_keywords[idiom]
+        keywords = self.src_keywords[idiom]
         keywords_present = []
         for keyword in keywords:
             keyword_present = []
             for w in keyword.split(';'):
-                # If the keyword is a Dutch word, we can't tell what the
-                # category is...
-                if w in self.nld_words:
-                    return "none"
-                keyword_present.append(w in prd)
+                keyword_present.append(w in nltk.word_tokenize(prd))
             keywords_present.append(any(keyword_present))
 
-        # If the keyword itself is present in the translation, return "copied"
+        # If the keyword itself is present in the translation, return "word-by-word"
         if any(keywords_present):
-            return "copied"
+            return "word-by-word"
 
         # Otherwise, check if the Dutch translation of the keyword is present
-        keywords = self.nl_keywords[idiom]
+        keywords = self.tgt_keywords[idiom]
         keywords_present = []
         for keyword in keywords:
-            keywords_present.append(any([w in prd for w in keyword.split(';')]))
+            keywords_present.append(
+                any([w in prd for w in keyword.split(';')]))
         if any(keywords_present):
             return "word-by-word"
 
@@ -75,17 +121,20 @@ class Classifier():
     def colour_keyword(self, idiom: str, src: str):
         """Mark the idiom's keywords in the source sentence for the survey."""
         # First colour the words that are the exact keyword
-        keywords = self.en_keywords[idiom]
+        keywords = self.src_keywords[idiom]
         new_idiom = idiom
         for w in keywords:
-            new_idiom = new_idiom.replace(w, f"<span style='color:red'><u>{w}</u></span>")
+            new_idiom = new_idiom.replace(
+                w, f"<span style='color:red'><u>{w}</u></span>")
 
         # Then colour the words that are the lemmas of the keyword
-        inflected = []
-        for w in keywords:
-            inflected.append(to_plural_singular(w)[1])
-        keywords = list(set(keywords + inflected))
-        keywords = [self.nlp_en(w)[0].lemma_ for w in keywords if w is not None]
+        # inflected = []
+        # for w in keywords:
+        #     inflected.append(self.to_plural_singular(w)[1])
+        # keywords = list(set(keywords + inflected))
+        # print(keywords)
+        keywords = [self.nlp_en(
+            w)[0].lemma_ for w in keywords if w is not None]
         src_coloured = []
         for w in src.split():
             lemmas = [x.lemma_.lower() for x in self.nlp_en(w)]
@@ -93,3 +142,14 @@ class Classifier():
                 w = f"<span style='color:red'><u>{w}</u></span>"
             src_coloured.append(w)
         return new_idiom, " ".join(src_coloured)
+
+    def to_plural_singular(self, word):
+        """Returns the plural form for singular inputs and vice versa."""
+        tokens = self.nlp_en(word)
+        if not inflect.singular_noun(tokens[0].text):
+            return tokens[0]._.inflect('NNS')
+        return tokens[0]._.inflect('NN')
+
+
+if __name__ == "__main__":
+    classifier = Classifier("idiom_keywords_translated_fr.tsv")
--- data/run_magpie.sh
+++ data/run_magpie.sh
@@ -1,42 +1,31 @@
 #!/bin/bash
 
-mkdir magpie
-mkdir magpie/inputs
+language=$1
 
-## These could be run, but the data is present in the repository.
-## Therefore, it's not needed to rerun.
-#python preprocess_json.py --pos_tag_method spacy --output_filename idiom_keywords.tsv --frequency_threshold 0.5 --samples_to_file
-#wait
-#python translate_keywords.py --input_tsv idiom_keywords.tsv --output_tsv idiom_keywords_translated.tsv
-#wait
-
-mkdir magpie/hidden_states_enc
-mkdir magpie/hidden_states_dec
-mkdir magpie/attention
-mkdir magpie/query_attention
-mkdir magpie/cross_attention
-mkdir magpie/prds
-mkdir magpie/tokenised_prds
+mkdir -p magpie/${language}
+mkdir -p magpie/${language}/hidden_states_enc
+mkdir -p magpie/${language}/attention
+mkdir -p magpie/${language}/query_attention
+mkdir -p magpie/${language}/cross_attention
+mkdir -p magpie/${language}/prds
+mkdir -p magpie/${language}/tokenised_prds
+mkdir -p magpie/${language}/mask_context
+mkdir -p magpie/${language}/mask_idiom
+mkdir -p magpie/${language}/mask_regular
 
 # To collect all data involves a lot of gigabytes, but this will provide you with a nice small subset of the data
 # that can be used to illustrate our findings
-for i in {0..1726..25}
+for i in {0..1726}
 do
-	# Files will be stored in magpie/prds/... magpie/hidden_states_enc/... etc.
-	python translate_magpie.py --source magpie/inputs/${i}.tsv --pred ${i}_pred.txt --folder magpie
+	echo "---------- IDIOM ${i} ----------"
+	# Files will be stored in magpie/LANGUAGE/prds/... magpie/LANGUAGE/hidden_states_enc/... etc.
+	python translate_magpie.py --source magpie/inputs/${i}.tsv --pred ${i}_pred.txt --folder magpie/${language} --language $language
 	wait
 done
 
-python label_magpie.py
+python translate_masking.py --source magpie/inputs/${i}.tsv --pred ${i}_pred.txt --folder magpie/${language} --mode mask_regular --language $language
 wait
-
-for i in {0..1726..25}
-do
-	# Files will be stored in magpie/masked_regular/..., etc.
-	python translate_masking.py --source magpie/inputs/${i}.tsv --pred ${i}_pred.txt --folder magpie --mode mask_regular --folder magpie
-	wait
-	python translate_masking.py --source magpie/inputs/${i}.tsv --pred ${i}_pred.txt --folder magpie --mode mask_context --folder magpie
-	wait
-	python translate_masking.py --source magpie/inputs/${i}.tsv --pred ${i}_pred.txt --folder magpie --mode mask_idiom --folder magpie
-	wait
-done
+python translate_masking.py --source magpie/inputs/${i}.tsv --pred ${i}_pred.txt --folder magpie/${language} --mode mask_context --language $language
+wait
+python translate_masking.py --source magpie/inputs/${i}.tsv --pred ${i}_pred.txt --folder magpie/${language} --mode mask_idiom --language $language
+wait
