bed611035af05fc3c76ca3b46cce5d3afd631ce2
Verna <vernadankers@gmail.com>
2022-05-30 02:18:25 +0100

    Updated old files

--- amnesic_probing/run_probing.sh
+++ amnesic_probing/run_probing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python probing.py --start 0 --stop 1727 --step 1 --setup hidden --target magpie --split_by_idiom --language $1
+python probing.py --start 0 --stop 1727 --step 1 --target magpie --split_by_idiom --language $1
 wait
-python probing.py --start 0 --stop 1727 --step 1 --setup hidden --target both --split_by_idiom --language $1
+python probing.py --start 0 --stop 1727 --step 1 --target both --split_by_idiom --language $1
 wait
